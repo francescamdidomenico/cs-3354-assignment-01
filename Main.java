a4b7f13ade61679d0f43cdc49449ffdd39bbcae3
Anup <anooopbasnet@gmail.com>
2026-02-18 19:12:29 -0600

    Completed Task 2: restockItem method

--- Main.java
+++ Main.java
@@ -23,4 +23,26 @@ public class Main {
         }
     }
 
+    /**
+     * Loops over the name array to find a target name, add amount to stocks at that index if found
+     * @author Anup Basnet
+     * @param names Array of item names
+     * @param stocks Array of stock quantities
+     * @param target Name of the item to find
+     * @param amount Quantity to add
+     */
+    public static void restockItem(String[] names, int[] stocks, String target, int amount){
+        boolean found = false;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null && names[i].equalsIgnoreCase(target)) {
+                stocks[i] += amount;
+                found = true;
+                break; 
+            }
+        }
+        if (!found) {
+            System.out.println("Item not found.");
+        }
+    }
+
 }
