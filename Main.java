7172631edfb93956a968572d44e77166bb70c125
Jack Doherty <jack10doherty@gmail.com>
2026-02-19 16:07:58 -0600

    Add javadoc comments for main class, implement task 3 menu

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
+/**
+ * Grocery management system using parallel arrays for item names, prices, and stocks.
+ * Features a menu to allow the user to display inventory and restock items.
+ */
+
 public class Main {
     
     /**
@@ -45,4 +52,57 @@ public class Main {
         }
     }
 
+
+    /**
+     * Main method of the class. Initializes inventory arrays and runs a loop 
+     * that allows the user to view inventory, restock items, or exit.
+     * @author Jack Doherty
+     */
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("--- Menu ---");
+            System.out.println("Choose one of the following options:");
+            System.out.println("1: View");
+            System.out.println("2: Restock");
+            System.out.println("3: Exit");
+
+            String input = sc.nextLine().trim();
+
+            if (input.equals("1")) {
+                printInventory(itemNames, itemPrices, itemStocks);
+            }
+            
+            else if (input.equals("2")) {
+                System.out.println("Enter the name of the item to restock: ");
+                String target = sc.nextLine().trim();
+
+                System.out.println("Enter the amount to add: ");
+                String amountString = sc.nextLine().trim();
+
+                int amount = Integer.parseInt(amountString);
+
+                if (amount <= 0) {
+                    System.out.println("Invalid amount. Amount must be greater than zero.");
+                    continue;
+                }
+
+                restockItem(itemNames, itemStocks, target, amount);
+                System.out.println();
+
+            }
+
+            else if (input.equals("3")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+        }
+
+        sc.close();
+    }
 }
